6a7fc246b0f0c1a600db3f47ffac31299116c98a
dsimon21 <dsimon21@nscds.org>
2020-01-09 15:44:00 +0000

    keydown switch

--- Buzzer Challenge/Buzzer.js
+++ Buzzer Challenge/Buzzer.js
@@ -1,1 +1,36 @@
+var shiftPressed = false;
+var elsePressed = false;
 
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 65: // a
+            if(!shiftPressed && !elsePressed){
+                elsePressed=true;
+
+            }
+        break;
+
+        case 83: // up
+        break;
+
+        case 68: // right
+        break;
+
+        case 70: // down
+        break;
+
+        case 16: // shift
+            shiftPressed=true;
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+$(document).keyup(function(e) {
+    if(e.which==16){
+        shiftPressed=false;
+    }
+})
