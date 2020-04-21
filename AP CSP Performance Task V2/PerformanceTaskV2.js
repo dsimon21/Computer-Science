3573d8dfd3fc7175fad738b344ab2e5711c71bd3
dsimon21 <dsimon21@nscds.org>
2020-04-21 17:05:25 +0000

    improvements

--- AP CSP Performance Task V2/PerformanceTaskV2.js
+++ AP CSP Performance Task V2/PerformanceTaskV2.js
@@ -10,26 +10,63 @@ $(document).ready(function(){
         var skater = ($("#nameBox").val()).toLowerCase();
         var coaches = ["coach", "coach1", "coach2", "coach3"]
         
+        function checkSkater(line) {
+            return line.s == skater;
+        }
+        
         function rule1(){
             console.log("rule1");
+            var x = 0;
+            var noCoach = false;
+            var curName = "";
+            while (!noCoach && x<=coaches.length-1 && x<=line.length-1){
+                curName = line[x].s
+                if(coaches.includes(curName)){
+                    x++;
+                }
+                else{
+                    line.splice(x,0,{s: skater, a: true});
+                    noCoach=true;
+                }
+            }
+            if(!noCoach && x==line.length-1){
+                line.push({s: skater, a: true});
+            }
         } 
+
 
         function rule2(){
             console.log("rule2");
+            var p=line.length-1;
+            var firstTurn=false;
+
+            function checkTurn(line) {
+                return line[p].s == removedName;
+            }
+
+            while (!firstTurn && p>=0){
+                removedName=line[p].s;
+                fullRemovedName=line[p];
+                line.splice(p,1);
+                if(line.findIndex(checkTurn)<p && line.findIndex(checkTurn)>=0){
+                    line.splice(p,0,fullRemovedName);
+                    p--;
+                }
+                else{
+                    line.splice(p,0,fullRemovedName);
+                    line.splice(p+1,0,{s: skater, a: true});
+                    firstTurn=true;
+                }       
+            }
+            console.log(line);
         }
          
         function rule3(){
             console.log("rule3")
             line.push({s: skater, a: true});
         }
-
-        function checkSkater() {
-            return line.s == skater;
-        }
         
         if((line.findIndex(checkSkater))>=0 || line.length==0){
-            console.log(line);
-            console.log(line.findIndex(checkSkater));
             rule3();
         }
         else{
@@ -41,6 +78,8 @@ $(document).ready(function(){
             }
         }    
     }
+
+    console.log(line);
 
         /*for(var p=0; p<line.length; p++){
             $('#listBox').append("<div class='nameItem'>" + line[p] + "</div>");
